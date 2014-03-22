a8eb9528b2640ff4499f6684a52b6caa9cbe5ea2
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-22 16:44:02 -0400

    fetches from Etsy now successfully dumping JSON to MongoDB

--- server.js
+++ server.js
@@ -21,22 +21,49 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-// ------ ETSY STUFF
+// ------ Gather cheapItems collection data
+var cheapItems = db.collection('cheapItems');
 
-request('https://openapi.etsy.com/v2/listings/active?api_key=gpby6hrhuzepnv0rx17946lk', function (error, response, body) {
+// clear existing data (temporary for testing)
+cheapItems.remove({}, function(err, result) {
+    if (!err) console.log('Cleared cheapItems collection!');
+});
+
+request({uri: 'https://openapi.etsy.com/v2/listings/active?&tags=art,painting&min_price=40&max_price=200&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(JSON.parse(body));
+    cheapItems.insert(body, function(err, result) {
+        if (err) throw err;
+        if (result) console.log('Fresh fetch to cheapItems collection!');
+    });
+  }
+})
+
+// ------ Gather expensiveItems collection data
+var expensiveItems = db.collection('expensiveItems');
+
+// clear existing data (temporary for testing)
+expensiveItems.remove({}, function(err, result) {
+    if (!err) console.log('Cleared expensiveItems collection!')
+});
+
+request({uri: 'https://openapi.etsy.com/v2/listings/active?&tags=art,painting&min_price=250&max_price=3000&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    expensiveItems.insert(body, function(err, result) {
+        if (err) throw err;
+        if (result) console.log('Added to expensiveItems collection!');
+    });
   }
 })
 
 
 // // ------ DUMMY MONGO
-// var cheapItems = db.collection('cheapItems');
 
 // cheapItems.find({title:'Dummy Painting'}).toArray(function(err, result) {
 //     console.log(err);
 //     console.log(result[0]);
 // });
+
+
 
 
 
