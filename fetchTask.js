d2c20666374490a9dd6596359e9f9d35118e5ba3
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-24 14:39:31 -0400

    fetches, obj filtering, DB integration working. Need to get Etsy pagination calls working (currently maxes at 100

---
+++ fetchTask.js
@@ -0,0 +1,1 @@
+// ------ Heroku scheduled task to update the Etsy DBs every X hours.
--- server.js
+++ server.js
@@ -21,65 +21,85 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-// ------ Gather cheapItems collection data
+// ------ FETCH FUNCTIONS
+
 var cheapItems = db.collection('cheapItems');
-
-// clear existing data (temporary for testing)
-cheapItems.remove({}, function(err, result) {
-    if (!err) console.log('Cleared cheapItems collection!');
-});
-
-request({uri: 'https://openapi.etsy.com/v2/listings/active?&limt=2&tags=art,painting&min_price=40&max_price=200&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    cheapItems.insert(body, function(err, result) {
-        if (err) throw err;
-        if (result) console.log('Fresh fetch to cheapItems collection!');
-    });
-  }
-})
-
-// ------ Gather expensiveItems collection data
 var expensiveItems = db.collection('expensiveItems');
-var etsyResults = [];
 
-// clear existing data (temporary for testing)
-expensiveItems.remove({}, function(err, result) {
-    if (!err) console.log('Cleared expensiveItems collection!')
-});
-
-request({uri: 'https://openapi.etsy.com/v2/listings/active?&limit=2&tags=art,painting&min_price=250&max_price=3000&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    expensiveItems.insert(body, function(err, result) {
-        if (err) throw err;
-        if (result) {
-            console.log('Fresh fetch to expensiveItems collection!');
-
-            expensiveItems.find({}).toArray(function(err, result) {
-                etsyResults = result[0].results;
-                console.log(etsyResults);
-            });
-
-            console.log(etsyResults);
-            // callback/promise needs to be implemented to prevent this from firing too soon.
-            // _.each(etsyResults, function(el, i) {
-            // });
-        }
+function deleteAllCheapItems() {
+    cheapItems.remove({}, function(err, result) {
+        if (!err) console.log('Removed all items from cheapItems collection!');
     });
-  }
-})
+}
 
+function deleteAllExpensiveItems() {
+    expensiveItems.remove({}, function(err, result) {
+        if (!err) console.log('Removed all items from expensiveItems collection!');
+    });
+}
 
+function fetchCheapItems() {
+    request({uri: 'https://openapi.etsy.com/v2/listings/active?&limit=1000&offset=100&tags=art,painting&min_price=40&max_price=110&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
+            //get just the results array from the Etsy return object
+            var bodyResults = [];
+            bodyResults = body.results;
 
-// // ------ DUMMY MONGO
+            //trim out select props from JSON response and push new obj to Mongo
+            _.each(bodyResults, function(el, i) {
+                var trimmedEtsyItem = {
+                    title : el.title,
+                    price : el.price,
+                    url : el.url,
+                    url_75x75 : el.Images[0].url_75x75,
+                    url_170x135 : el.Images[0].url_170x135,
+                    url_570xN : el.Images[0].url_570xN,
+                    url_fullxfull : el.Images[0].url_fullxfull
+                };
+                //save to Mongo
+                cheapItems.insert(trimmedEtsyItem, function(err, result) {
+                    if (err) throw err;
+                    if (result) console.log('added to cheapItems!');
+                });
+            })
+        }
+    })
+}
 
-// cheapItems.find().toArray(function(err, result) {
-//     console.log(err);
-//     console.log(result);
-// });
+function fetchExpensiveItems() {
+    request({uri: 'https://openapi.etsy.com/v2/listings/active?&limit=1000&offset=100&tags=art,painting&min_price=250&max_price=3000&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
+            //get just the results array from the Etsy return object
+            var bodyResults = [];
+            bodyResults = body.results;
 
+            //trim out select props from JSON response and push new obj to Mongo
+            _.each(bodyResults, function(el, i) {
+                var trimmedEtsyItem = {
+                    title : el.title,
+                    price : el.price,
+                    url : el.url,
+                    url_75x75 : el.Images[0].url_75x75,
+                    url_170x135 : el.Images[0].url_170x135,
+                    url_570xN : el.Images[0].url_570xN,
+                    url_fullxfull : el.Images[0].url_fullxfull
+                };
+                //save to Mongo
+                expensiveItems.insert(trimmedEtsyItem, function(err, result) {
+                    if (err) throw err;
+                    if (result) console.log('added to expensiveItems!');
+                });
+            })
+        }
+    })
+}
 
+deleteAllCheapItems();
+deleteAllExpensiveItems();
+fetchCheapItems();
+fetchExpensiveItems();
 
 
 
