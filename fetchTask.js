762057efc587498df1ca794fec6c5a1e541e30cb
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-30 21:51:08 -0400

    integrated base Express app assets

--- fetchTask.js
+++ fetchTask.js
@@ -1,1 +1,6 @@
 // ------ Heroku scheduled task to update the Etsy DBs every X hours.
+
+//fetch everything first
+//organize them into 'quiz pairs'
+//then in a promise/callback, delete existing data (this shouldn't be an issue for people currently on the site as )
+//then insert the fetch
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var request = require('request'),
     express = require("express"),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
     app = express(),
     _ = require('underscore'),
     mongo = require('mongoskin'),
@@ -14,17 +17,38 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-// ------ Dev/Production Port ------
+// ------ BOILERPLATE EXPRESS ------
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// all environments
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 // ------ FETCH FUNCTIONS ------
 
 var cheapItems = db.collection('cheapItems');
 var expensiveItems = db.collection('expensiveItems');
+
+var cheapItemsArray = [];
+var expensiveItemsArray = [];
+var quizPairs = [];
 
 function deleteAllCheapItems() {
     cheapItems.remove({}, function(err, result) {
@@ -42,7 +66,7 @@ function fetchCheapItems(howManyHundred) {
     var theLimit = howManyHundred;
     var theOffset = 0;
 
-    for (var i = 0; i < theLimit; i += theOffset) {
+    for (var i = 0; i < theLimit; i += 100) {
         etsyCheapItems(theLimit, theOffset);
         theOffset += 100;
     }
@@ -52,7 +76,7 @@ function fetchExpensiveItems(howManyHundred) {
     var theLimit = howManyHundred;
     var theOffset = 0;
 
-    for (var i = 0; i < theLimit; i += theOffset) {
+    for (var i = 0; i < theLimit; i += 100) {
         etsyExpensiveItems(theLimit, theOffset);
         theOffset += 100;
     }
@@ -78,10 +102,8 @@ function etsyCheapItems(limit, offset) {
                     url_570xN : el.Images[0].url_570xN,
                     url_fullxfull : el.Images[0].url_fullxfull
                 };
-                //save to Mongo
-                cheapItems.insert(trimmedEtsyItem, function(err, result) {
-                    if (err) throw err;
-                });
+                //save to cheapItemsArray
+                cheapItemsArray.push(trimmedEtsyItem);
             })
         }
     })
@@ -89,7 +111,7 @@ function etsyCheapItems(limit, offset) {
 
 function etsyExpensiveItems(limit, offset) {
 
-    request({uri: 'https://openapi.etsy.com/v2/listings/active?&limit='+limit+ '&offset=' +offset+ '&tags=art,painting&min_price=40&max_price=110&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
+    request({uri: 'https://openapi.etsy.com/v2/listings/active?&limit='+limit+ '&offset=' +offset+ '&tags=art,painting&min_price=250&max_price=1000&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk', json: true}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
 
             //get just the results array from the Etsy return object
@@ -107,21 +129,34 @@ function etsyExpensiveItems(limit, offset) {
                     url_570xN : el.Images[0].url_570xN,
                     url_fullxfull : el.Images[0].url_fullxfull
                 };
-                //save to Mongo
-                expensiveItems.insert(trimmedEtsyItem, function(err, result) {
-                    if (err) throw err;
-                });
+                //save to expensiveItemsArray
+                expensiveItemsArray.push(trimmedEtsyItem);
             })
         }
     })
 }
 
+function generateQuizPairs() {
+    cheapItemsArray = _.shuffle(cheapItemsArray);
+    expensiveItemsArray = _.shuffle(expensiveItemsArray);
+
+    for (var i = 0; i < cheapItemsArray.length; i++) {
+        var staging = [expensiveItemsArray[i], cheapItemsArray[i]];
+        staging = _.shuffle(staging);
+
+        //put the shuffled 'mini-array' pair in the actual array
+        quizPairs.push(staging);
+    }
+}
+
 // ------ KICKOFF! ------
 deleteAllCheapItems();
 deleteAllExpensiveItems();
-fetchCheapItems(1000);
-fetchExpensiveItems(1000);
+fetchCheapItems(500);
+fetchExpensiveItems(500);
 
+setInterval(function() {generateQuizPairs();}, 20000);
+setInterval(function() {console.log(quizPairs); console.log(quizPairs.length);}, 25000);
 
 
 
